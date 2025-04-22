4d101037b7938d36d136d3446b84923b6eef4742
Eragam Reddy N <neragamreddy2002@gmail.com>
2025-04-22 09:24:57 +0530

    Add files via upload

---
+++ Basic_of_Fun.js
@@ -0,0 +1,32 @@
+// Function are very important
+//Functions are one of the datatypes in js
+// a function return another function
+
+// if funtion inside another funtion we can call "outer()()"
+
+
+// function add(x,y){
+//     console.log(x()+y);
+// }
+// add(function(){
+//     return 5
+// },4)
+
+
+// Higher Order function:-
+    //a funtion accepting another funtion as a argument is know as HOF
+// general funtions
+// x=10;
+// console.log(x)
+// let x
+
+let abc= ()=>{
+    console.log('Helo')
+
+}
+abc()
+
+
+
+
+   
